e7c0570db22c963b7fd3069eba3733c947f24788
Alex Musteata <Alex.Musteata-3rdpty@brooksautomation.com>
2024-12-14 19:41:10 +0200

    add password changes

--- src/app/commons/enums/shared-enums.ts
+++ src/app/commons/enums/shared-enums.ts
@@ -2,7 +2,3 @@ export enum Theme {
   LIGHT = 'light',
   DARK = 'dark',
 }
-export enum InputType {
-  TEXT = 'text',
-  PASSWORD = 'password',
-}
--- src/app/components/common-ui/input-element/input-element.component.ts
+++ src/app/components/common-ui/input-element/input-element.component.ts
@@ -1,19 +1,42 @@
-import {ChangeDetectionStrategy, Component, Input, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, signal, SimpleChanges} from "@angular/core";
+import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-input-element',
+  selector: "app-input-element",
   standalone: true,
-  imports: [],
-  templateUrl: './input-element.component.html',
-  styleUrl: './input-element.component.scss',
+  imports: [
+    NgIf
+  ],
+  templateUrl: "./input-element.component.html",
+  styleUrl: "./input-element.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InputElementComponent {
+export class InputElementComponent implements OnChanges {
+  //region Inputs
   @Input() placeholder: { description: string; action: string } = {
-    description: 'Description',
-    action: 'Please enter description'
+    description: "Description",
+    action: "Please enter description"
   };
-  @Input() iconPath: string = 'assets/icons/user-icon.svg#user-icon';
+  @Input() iconPath: string = "assets/icons/user-icon.svg#user-icon";
+  @Input() typeInput: string = "text";
+  //endregion
+  //region Signals
   focused = signal<boolean>(false);
   notEmpty = signal<boolean>(false);
+  inputType = signal<string>(this.typeInput);
+  //endregion
+  //region Methods
+  togglePassword() {
+    this.inputType.set(
+      this.inputType() === "password" ? "text" : "password")
+  }
+  //endregion
+  //region Overrides
+  ngOnChanges(changes: SimpleChanges): void {
+    this.placeholder = changes["placeholder"] ? changes["placeholder"].currentValue ?? this.placeholder : this.placeholder;
+    this.iconPath = changes["iconPath"] ? changes["iconPath"].currentValue ?? this.iconPath : this.iconPath;
+    this.typeInput = changes["typeInput"] ? changes["typeInput"].currentValue ?? this.typeInput : this.typeInput;
+    this.inputType.set(this.typeInput);
+  }
+  //endregion
 }
